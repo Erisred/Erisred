f6b1b2a3354d525c6e270e35b9cabc0cc2673db5
Erisred <linke505@live.com>
2022-05-25 13:39:36 -0500

    initial

---
+++ _site/README.md
@@ -0,0 +1,14 @@
+### Hi there 👋
+
+**Erisred/Erisred** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on my GitHub profile.
+
+Yep, this is the stock page... I save my creativity for elsewhere!
+
+- 🔭 I’m currently working on ... an [Obsidian](https://obsidian.md/) PKB, and starting a blog, meant for personal use, mainly
+- 🌱 I’m currently (re)learning ... TiddlyWiki. I'm trying it out as a blog platform.
+- 👯 I’m looking to collaborate on ... Unsure. I've recently completed my first-ever PR, and it went pretty smoothly.
+- 🤔 I’m looking for help with ... Nothing, currently - but I'm sure I'll find a forum somewhere when I do.
+- 💬 Ask me about ... 4-H and Horses!
+- 📫 How to reach me: ... Private for now.
+- 😄 Pronouns: ... I'm guessing at the format, here, but He/Him/His?
+- ⚡ Fun fact: ... I didn't get a haircut throughout most of high school. I cut it when my daughter was around 2. At the time I could put it in the pocket of my jeans.
---
+++ _site/assets/js/toggletheme.js
@@ -0,0 +1,32 @@
+// toggle from light mode to dark mode
+// requires css variables defined in :root, then a set of alternate variables under body.light-theme that will be active when the link is clicked
+
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+// Select the theme preference from localStorage
+const currentTheme = localStorage.getItem("theme");
+// Select the text fo the link
+var toggleText = document.getElementById("btn-toggle");
+
+// If the current theme in localStorage is "dark"...
+if (currentTheme == "light") {
+  // ...then use the .dark-theme class
+  document.body.classList.add("light-theme");
+  toggleText.innerHTML = "Light theme";
+}
+
+// Listen for a click on the button 
+btn.addEventListener("click", function() {
+  // Toggle the .dark-theme class on each click
+  document.body.classList.toggle("light-theme");
+  
+  // Let's say the theme is equal to light
+  let theme = "dark";
+  // If the body contains the .dark-theme class...
+  if (document.body.classList.contains("light-theme")) {
+    // ...then let's make the theme dark
+    theme = "light";
+  }
+  // Then save the choice in localStorage
+  localStorage.setItem("theme", theme);
+});
