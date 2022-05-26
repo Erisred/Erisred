cff85c0a21208a3a474f08baf4985ad521307493
Erisred <linke505@live.com>
2022-05-25 19:54:59 -0500

    nav

--- assets/js/toggletheme.js
+++ assets/js/toggletheme.js
@@ -15,12 +15,18 @@ if (currentTheme == "light") {
   toggleText.innerHTML = "Light theme";
 }
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+// Listen for a click on the button 
+btn.addEventListener("click", function() {
+  // Toggle the .dark-theme class on each click
+  document.body.classList.toggle("light-theme");
+  
+  // Let's say the theme is equal to light
+  let theme = "dark";
+  // If the body contains the .dark-theme class...
+  if (document.body.classList.contains("light-theme")) {
+    // ...then let's make the theme dark
+    theme = "light";
   }
-}
+  // Then save the choice in localStorage
+  localStorage.setItem("theme", theme);
+});
